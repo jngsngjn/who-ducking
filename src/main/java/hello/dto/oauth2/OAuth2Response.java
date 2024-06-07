b1f7822ffc94a1027bf998f9b8fc010fccad6c11
jngsngjn <wjdtjdwls98@gmail.com>
2024-06-08 01:16:24 +0900

    주석 추가

--- src/main/java/hello/dto/oauth2/OAuth2Response.java
+++ src/main/java/hello/dto/oauth2/OAuth2Response.java
@@ -2,10 +2,10 @@ package hello.dto.oauth2;
 
 public interface OAuth2Response {
 
-    // 제공자 (naver, google, ...)
+    // 제공자 (naver, google, kakao)
     String getProvider();
 
-    // 제공자에서 발급해주는 아이디(번호)
+    // 제공자에서 발급해 주는 아이디 (UNIQUE)
     String getProviderId();
 
     String getEmail();
