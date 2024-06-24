5cc51b3334f0cca117df833ef293b121d2cad1a0
Booreung <120548686+Booreung@users.noreply.github.com>
2024-06-24 17:27:23 +0900

    reviewBoard-image-fix

--- src/main/java/hello/dto/animation/GetAniListDTO.java
+++ src/main/java/hello/dto/animation/GetAniListDTO.java
@@ -9,13 +9,13 @@ import lombok.Setter;
 @Setter
 public class GetAniListDTO {
     private long animationId;
-    private String imagePath;
+    private String imageName;
     private Double score;
     private long reviewCount;
 
-    public GetAniListDTO(long animationId, String imagePath, Double score, long reviewCount) {
+    public GetAniListDTO(long animationId, String imageName, Double score, long reviewCount) {
         this.animationId = animationId;
-        this.imagePath = imagePath;
+        this.imageName = imageName;
         this.score = score;
         this.reviewCount = reviewCount;
     }
