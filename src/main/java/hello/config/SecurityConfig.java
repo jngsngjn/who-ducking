f5360512ed2884fc2ae5ec89e85446dbec195b7a
jngsngjn <wjdtjdwls98@gmail.com>
2024-06-26 16:09:34 +0900

    security 수정 - 기초 작업

--- src/main/java/hello/config/SecurityConfig.java
+++ src/main/java/hello/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/oauth2/**", "/login", "/register/**").permitAll()
+
                 .requestMatchers("/css/**", "/jpg/**", "/png/**", "/js/**", "/images/**", "/vendor/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
--- src/main/java/hello/controller/notice/NoticeController.java
+++ src/main/java/hello/controller/notice/NoticeController.java
@@ -7,25 +7,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/notice")
 @RequiredArgsConstructor
-public class NoticePageController {
+public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @GetMapping
+    @GetMapping("/announcement")
     public String notice(Model model, @RequestParam(name = "page", defaultValue = "0") int page) {
         Page<NoticeDTO> noticePage = noticeService.getNoticePage(page, 5);
         model.addAttribute("notices", noticePage);
-        return "/notice/noticePage";
+        return "notice/announcement";
     }
 
-    @GetMapping("/faqPage")
+    @GetMapping("/faq")
     public String faqPage() {
-        return "/notice/faqPage";
+        return "notice/faq";
     }
 }
