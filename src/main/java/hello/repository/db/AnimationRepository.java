60928a1a0c8071d63c0fa56086251de24b4186eb
kimsu10 <betnwjd25@gmail.com>
2024-06-28 20:24:13 +0900

    FIX: 애니리스트 리뷰수 2배 출력 오류 해결

--- src/main/java/hello/repository/db/AnimationRepository.java
+++ src/main/java/hello/repository/db/AnimationRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface AnimationRepository extends JpaRepository<Animation, Long> {
 
     // 애니 전체 리스트 조회 -> 최신순으로 조회 + 장르 id 추가
-    @Query("SELECT new hello.dto.animation.GetAniListDTO(a.id, a.imageName, ROUND(COALESCE(AVG(r.score), 0.0), 1), COUNT(r.id), a.name, " +
+    @Query("SELECT new hello.dto.animation.GetAniListDTO(a.id, a.imageName, ROUND(COALESCE(AVG(r.score), 0.0), 1), COUNT(DISTINCT r.id), a.name, " +
             "MIN(ag.genre.id), MAX(ag.genre.id)) " +
             "FROM Animation a " +
             "LEFT JOIN a.reviews r " +
@@ -22,7 +22,7 @@ public interface AnimationRepository extends JpaRepository<Animation, Long> {
     List<GetAniListDTO> findAnimationsWithReviews();
 
 
-    // 애니 별 평점과 리뷰수 조회
+    // 애니상세리스트 별 평점과 리뷰수 조회
     @Query("SELECT new hello.dto.animation.GetAniListDTO(a.id, NULL, ROUND(COALESCE(AVG(r.score), 0.0), 1), COUNT(r.id), a.name) " +
             "FROM Animation a LEFT JOIN a.reviews r " +
             "WHERE a.id = :animationId " +
