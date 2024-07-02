c97e9587142eea2cfa7ea4e8c9fc5d6f54f3e05b
jngsngjn <wjdtjdwls98@gmail.com>
2024-07-02 11:18:02 +0900

    [검색] - 검색 텍스트 유지 및 삭제

--- src/main/webapp/js/fragments/header.js
+++ src/main/webapp/js/fragments/header.js
@@ -243,3 +243,26 @@ function deleteAlarm(alarmId) {
         }
     });
 }
+
+$(document).ready(function() {
+    // 페이지 로드 시 검색 인풋에 로컬 스토리지 값을 설정
+    var searchInput = localStorage.getItem('searchInput');
+    if (searchInput) {
+        $('#searchInput').val(searchInput);
+    }
+
+    // 검색 폼 제출 시 검색 인풋 값을 로컬 스토리지에 저장
+    $('.h-search_box').on('submit', function() {
+        var searchValue = $('#searchInput').val();
+        localStorage.setItem('searchInput', searchValue);
+    });
+
+    // 특정 탭 클릭 시 로컬 스토리지의 검색 인풋 값을 삭제
+    $('.h-gnb_items a').on('click', function() {
+        localStorage.removeItem('searchInput');
+    });
+
+    $('.navbar-brand').on('click', function() {
+        localStorage.removeItem('searchInput');
+    });
+});
